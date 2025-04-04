c703d9b6ba64166c278b00e2c126d6fe4a0eecbd
Jacob <jacobguerrant01@gmail.com>
2025-04-04 18:36:19 -0400

    feat(api)!: pulls weather and assigns icon

--- src/App.js
+++ src/App.js
@@ -22,21 +22,50 @@ import {
   BsThermometer
 } from 'react-icons/ti';
 
-import TbTemperatureFa from 'react-icons/tb';
-import ImSpinner2 from 'react-icons/im';
-import IoSearch from 'react-icons/io';
+import { TbTemperatureFa } from 'react-icons/tb';
+import { ImSpinner2 } from 'react-icons/im';
+import { IoSearch } from 'react-icons/io';
 
 
 const App = () => {
   const [data, setData] = useState(null);
   const [city, setCity] = useState('London');
 
-  // Fetch data
+  // Fetch data with OWM
   useEffect(() => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_OWM_API_KEY}`;
-  })
 
-  return <div>react app</div>;
+    axios.get(url).then(res => {
+      setData(res.data);
+    });
+  }, [city]);
+
+  // Display loading while data fetch in progress
+  if (!data) {
+    return (
+      <div>
+        <div>
+          <ImSpinner2 className='text-5x1' />
+        </div>
+      </div>
+    )
+  }
+
+  // Corresponding weather icon handling
+  let icon;
+  //console.log(data.weather[0].main);
+  switch (data.weather[0].main) {
+    case 'Clouds':
+      icon = <TiWeatherCloudy />;
+      break
+  }
+  // TODO: Fill rest of weather case conditions
+
+  return <div>react app
+    <div>
+      <div className='text-6x1'>{icon}</div>
+    </div>
+  </div>;
 };
 
 export default App;
