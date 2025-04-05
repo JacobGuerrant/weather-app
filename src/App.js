837930fa3579c56acb6f1ffb5f2aaf289b925712
Jacob <jacobguerrant01@gmail.com>
2025-04-04 20:14:50 -0400

    feat!: Weather information UI element added

--- src/App.js
+++ src/App.js
@@ -20,9 +20,9 @@ import {
   BsEye,
   BsWind,
   BsThermometer
-} from 'react-icons/ti';
+} from 'react-icons/bs';
 
-import { TbTemperatureFa } from 'react-icons/tb';
+import { TbTemperatureFahrenheit, TbTemperatureCelsius } from "react-icons/tb";
 import { ImSpinner2 } from 'react-icons/im';
 import { IoSearch } from 'react-icons/io';
 
@@ -31,9 +31,12 @@ const App = () => {
   const [data, setData] = useState(null);
   const [city, setCity] = useState('London');
 
+  //TODO: Support unit changing
+  const unit = 'Imperial';
+
   // Fetch data with OWM
   useEffect(() => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_OWM_API_KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=${process.env.REACT_APP_OWM_API_KEY}`;
 
     axios.get(url).then(res => {
       setData(res.data);
@@ -61,11 +64,78 @@ const App = () => {
   }
   // TODO: Fill rest of weather case conditions
 
-  return <div>react app
-    <div>
-      <div className='text-6x1'>{icon}</div>
+  // Uses Intl.DisplayNames to get full country name from OWM supplied country code
+  const regionNames = new Intl.DisplayNames(['en'], { type: 'region' });
+  const fullCountryName = regionNames.of(data.sys.country);
+
+  // Fetch user date and properly format
+  const date = new Date();
+  const month = (date.getUTCMonth() + 1).toString().padStart(2, '0'); // Add 1 because getUTCMonth is 0 indexed
+  const day = date.getUTCDate().toString().padStart(2, '0');
+
+  return <div className='g_bg flex flex-col items-center justify-center'>
+    {/* TODO: Location search */}
+    <form>Search</form>
+
+    {/* Card */}
+    <div className='w-full max-w-[450px] min-h-[400px] bg-black/20 text-white backdrop-blur-[32px] rounded-[32px] px-6 py-12'>
+      <div>
+
+        {/* Card top */}
+        <div className='flex items-center gap-x-5'>
+          {/* Icon */}
+          <div className='text-[87px]'>{icon}</div>
+          {/* Country */}
+          <div className='text-2xl font-semibold'>{data.name}, {fullCountryName}</div>
+          {/* Date */}
+          <div className=''>{month}/{day}/{date.getUTCFullYear()}</div>
+        </div>
+
+        {/* Card body */}
+        <div className='my-5'>
+          <div className='flex justify-center'>
+            {/* Temperature*/}
+            <div className='text-[72px] leading-none'>{parseInt(data.main.temp)}</div>
+            {/* Icon */}
+            <div className='text-4xl'>
+              <TbTemperatureFahrenheit />
+            </div>
+          </div>
+          {/* Weather description */}
+          <div className='text-center text-2xl'>{data.weather[0].main}</div>
+        </div>
+
+        {/* Card bottom */}
+        <div>
+          <div className='flex justify-between text-[20px]'>
+
+            <div className='flex items-center gap-x-2'>
+              {/* Icon */}
+              <div>
+                <BsWater />
+              </div>
+              {/* Humidity */}
+              <div>
+                Humidity:  {data.main.humidity}%
+              </div>
+            </div>
+            <div className='flex items-center gap-x-2'>
+              {/* Icon */}
+              <div>
+                <BsWind />
+              </div>
+              {/* Wind speed */}
+              <div>
+                Wind: {data.wind.speed} mph
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>;
+
+  // TODO: Eventually sync background gradient/image with local weather
 };
 
 export default App;
